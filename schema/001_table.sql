8890c419bc75a09bdfc1dc8192123e0351251fe1
Tolben <vitek.pasek@gmail.com>
2025-10-22 00:01:13 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/001_table.sql
+++ schema/001_table.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
 -- table: vat_validations
 CREATE TABLE IF NOT EXISTS vat_validations (
   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
--- schema/030_foreign_keys.sql
+++ schema/030_foreign_keys.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
 -- table: vat_validations
 ALTER TABLE vat_validations ADD CONSTRAINT fk_vat_validations_country FOREIGN KEY (country_iso2) REFERENCES countries(iso2) ON DELETE CASCADE;
